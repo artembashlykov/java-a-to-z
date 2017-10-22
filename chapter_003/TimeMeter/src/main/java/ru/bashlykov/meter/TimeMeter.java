cbb6e3e77b181f6fa243ce6fe42caf96c3c5cfa2
Art <a_bashlykov@inbox.ru>
2017-10-23 00:05:19 +0300

    finished task of measuring time of adding/removing elements of a collection.

---
+++ chapter_003/TimeMeter/src/main/java/ru/bashlykov/meter/TimeMeter.java
@@ -0,0 +1,27 @@
+package main.java.ru.bashlykov.meter;
+
+import java.util.*;
+
+public class TimeMeter {
+
+    public static long add(Collection<String> collection, String[] lines){
+        long start = System.nanoTime();
+        for(String col : lines) {
+            collection.add(col);
+        }
+        long traceTime = System.nanoTime() - start;
+
+        return traceTime;
+    }
+
+    public static long remove(Collection<String> collection){
+        long start = System.nanoTime();
+        Iterator<String> iter = collection.iterator();
+        while(iter.hasNext()){
+            iter.next();
+            iter.remove();
+        }
+        long traceTime = System.nanoTime() - start;
+        return traceTime;
+    }
+}
