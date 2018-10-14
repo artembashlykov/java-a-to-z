63e9cf0b65fef002a7100ff8f7770f3f456de113
artembashlykov <a_bashlykov@inbox.ru>
2018-10-14 21:50:41 +0300

    2. Очередь с приоритетом на LinkedList [#41670](главное помнить о break)

--- chapter_003/Priority/src/main/java/ru/abashlykov/task/PriorityQueue.java
+++ chapter_003/Priority/src/main/java/ru/abashlykov/task/PriorityQueue.java
@@ -5,23 +5,23 @@ import java.util.LinkedList;
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
-    public void put(Task task){
-
-        if(tasks.isEmpty()){
-            tasks.add(task);
-        } else if (!tasks.isEmpty()){
-            for(Task element : tasks){
-                if(element.getPriority() > task.getPriority()){
-                    tasks.add(element.getPriority(), task);                }
-            }
-        }
-
-
+    public void put(Task task) {
+       if(tasks.isEmpty()){
+           tasks.add(0, task);
+       } else {
+           for(Task element : tasks){
+               if(element.getPriority() > task.getPriority()){
+                   int numberOfElement = tasks.indexOf(element);
+                   tasks.add(numberOfElement, task);
+                   break;
+               }
+           }
+       }
     }
 
     public Task take(){
-        return this.tasks.pollFirst();
+        return this.tasks.poll();
     }
-
-
 }
+
+
