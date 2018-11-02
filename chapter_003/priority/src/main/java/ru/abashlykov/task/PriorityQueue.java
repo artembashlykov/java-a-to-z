fafeb59914f75692b78fa92cbb82258b27c4d12d
artembashlykov <a_bashlykov@inbox.ru>
2018-11-02 20:12:33 +0300

    2. Очередь с приоритетом на LinkedList [#41670][#76038] SUCCESS

--- chapter_003/priority/src/main/java/ru/abashlykov/task/PriorityQueue.java
+++ chapter_003/priority/src/main/java/ru/abashlykov/task/PriorityQueue.java
@@ -6,59 +6,16 @@ public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-//       if(tasks.isEmpty()){
-//           tasks.add(0, task);
-//       } else {
-//           for(Task element : tasks){
-//               if(element.getPriority() > task.getPriority()){
-//                   int numberOfElement = tasks.indexOf(element);
-//                   tasks.add(numberOfElement, task);
-//                   break;
-//               }
-//           }
-//       }
-//    for(Task element : tasks){
-//
-//        int taskIndex = tasks.indexOf(element);
-//
-//        if(tasks.isEmpty()){
-//            tasks.add(0, task);
-//            break;
-//        }
-//        if(element.getPriority() < task.getPriority()){
-//            taskIndex++;
-//        } else {
-//            break;
-//        }
-//        if(element.getPriority() > task.getPriority() & tasks.indexOf(element) != 0){
-//            taskIndex--;
-//        } else {
-//            break;
-//        }
-//        tasks.add(taskIndex, task);
-//    }
-//        int itemIndex = 0;
-//        for (int i = 0; i <= tasks.size(); i++ ){
-//            if(tasks.isEmpty()){
-//                break;
-//            }else if(tasks.get(i).getPriority() >= task.getPriority()){
-//                itemIndex = i;
-//                break;
-//            } else if(tasks.get(i).getPriority() < task.getPriority() & tasks.size()-1 > i){
-//                continue;
-//            } else if(tasks.get(i).getPriority() < task.getPriority() & tasks.size()-1 == i) {
-//               tasks.add(task);
-//            }
-//        }
-//        tasks.add(itemIndex, task);
-//        for(Task el : tasks){
-//            System.out.println(el.getDesc());
-//        }
-
-    }
-
-    public void vision(){
-
+        int itemIndex = 0;
+        for (int i = 0; i < tasks.size(); i++ ){
+            if(tasks.isEmpty()){
+                break;
+            }else if(tasks.get(i).getPriority() >= task.getPriority() | (i+1) == tasks.size()){
+                itemIndex = i;
+                break;
+            }
+        }
+        tasks.add(itemIndex, task);
     }
 
     public Task take(){
